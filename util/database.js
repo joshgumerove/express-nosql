da623e5c2cf9d75aa18a4352807754c84216a045
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-04-28 12:35:34 -0400

    additional mongo config

--- util/database.js
+++ util/database.js
@@ -1,13 +1,19 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect("mongodb://localhost:27017/mydatabase")
     .then((client) => {
       console.log("connected to mongo");
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch((err) => console.log("error connecting to mongo: ", err));
+    .catch((err) => {
+      console.log("error connecting to mongo: ", err);
+      throw err;
+    });
 };
 
 module.exports = mongoConnect;
