1907c150c0bc579927be0f8ea601f49188c03c2a
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-04-28 20:32:06 -0400

    find product by object id

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
 const getDb = require("../util/database").getDb;
+const mongodb = require("mongodb");
 
 class Product {
   constructor(title, price, description, imageUrl) {
@@ -35,6 +36,21 @@ class Product {
       .catch((err) => {
         console.log("there is an error compass: ", err);
       });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log("here is the product: ", product);
+        return product;
+      })
+      .catch((err) =>
+        console.log("there was an error getting the product: ", err)
+      );
   }
 }
 
