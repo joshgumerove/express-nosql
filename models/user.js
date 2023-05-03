bee9cc844405d031c29270d581086dda231189ce
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-03 00:24:35 -0400

    create user

--- models/user.js
+++ models/user.js
@@ -1,16 +1,33 @@
-const Sequelize = require('sequelize');
+const getDb = require("../util/database").getDb;
+const mongodb = require("mongodb");
 
-const sequelize = require('../util/database');
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save() {
+    const db = getDb();
+    db.collection("users")
+      .insertOne(this)
+      .then((result) => console.log("user was created"))
+      .catch((err) => console.log("error creating a user: ", err));
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectId(userId) })
+      .then((user) => {
+        console.log("here is the user: ", user);
+        return user;
+      })
+      .catch((err) =>
+        console.log("there was an error getting the user: ", err)
+      );
+  }
+}
 
 module.exports = User;
