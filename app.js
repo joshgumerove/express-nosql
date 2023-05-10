f84d1b2927e633bca13f3385d1f303ef408a044c
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-10 12:45:19 -0400

    add single item to cart

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("6451e0e1abf61c3ec32b2a00")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.username, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
