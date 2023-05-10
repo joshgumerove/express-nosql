0c23d4ee316e6338c41bf8c241597eb29c25d445
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-10 11:57:34 -0400

    create cart and update functionality

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
 
   save() {
@@ -13,6 +15,20 @@ class User {
       .insertOne(this)
       .then((result) => console.log("user was created"))
       .catch((err) => console.log("error creating a user: ", err));
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db.collection("users").updateOne(
+      { _id: new mongodb.ObjectId(this._id) },
+      {
+        $set: { cart: updatedCart },
+      }
+    );
   }
 
   static findById(userId) {
