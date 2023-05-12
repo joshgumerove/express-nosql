76d34a9f54ca813681682a69c6caa7476687ca2b
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-11 21:38:31 -0400

    delete items from cart

--- models/user.js
+++ models/user.js
@@ -69,6 +69,20 @@ class User {
       });
   }
 
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter(
+      (items) => items.productId.toString() !== productId.toString()
+    );
+
+    const db = getDb();
+    return db.collection("users").updateOne(
+      { _id: new mongodb.ObjectId(this._id) },
+      {
+        $set: { cart: { items: updatedCartItems } },
+      }
+    );
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
