f925116b2cf3e5814cfb34b37f3cfd153a252ffa
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-13 01:17:19 -0400

    get cart with mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,9 +38,10 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  req.user
-    .getCart()
-    .then((products) => {
+  return req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items;
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
