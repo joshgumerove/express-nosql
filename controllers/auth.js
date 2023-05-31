29ecf621f5c74058386fb3ff9634078f1406b1e6
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-31 15:06:16 -0400

    set cookie initial logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,16 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get("Cookie").split(";")[3].trim().split("=")[1];
+
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: req.isLoggedIn,
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  res.setHeader("Set-Cookie", "loggedIn=true"); // NOTE: the name 'Set-Cookie' is a reserved header value
+  // NOTE: loggedIn=true creates key of loggedIn and value of true
+  // can see in Application/storage/cookies
   res.redirect("/");
-}; // note: form action=/post will be handled by this route
+}; // NOTE: form action = /post will be handled by this route
